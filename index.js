3755d8c2a58dfc87a34ba95a7e24f8312941744f
belabastos-art <mb10491@nyu.edu>
2025-10-15 09:51:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-{
-  "name": "homeworkserver",
-  "version": "1.0.0",
-  "description": "homerwork",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "isabela",
-  "license": "ISC",
-  "dependencies": {
-    "express": "^5.1.0"
-  }
-}
+
