5a826f5683bad9430851d090fc2afe361ff4549f
belabastos-art <mb10491@nyu.edu>
2025-10-15 09:44:13 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+{
+  "name": "homeworkserver",
+  "version": "1.0.0",
+  "description": "homerwork",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "isabela",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^5.1.0"
+  }
+}
